398c1f93bad936d18fb20d403eca64c55af7c667
Joachim Hill-Grannec <joachim@liffft.com>
2016-06-21 11:41:16 -0700

    remove unused import

--- plugin.go
+++ plugin.go
@@ -7,7 +7,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/elasticbeanstalk"
-	"os"
 )
 
 // Plugin defines the beanstalk plugin parameters.
